acd2c625e45d3d461060bfa8143580d5c2c436fe
gmjishara <janith971231@gmail.com>
2023-06-23 20:02:27 +0530

    set half of ageCalculate function

---
+++ script.js
@@ -0,0 +1,8 @@
+const ageCalculate = () => {
+  const today = new Date();
+  const birthday = new Date(document.getElementById("birthday"));
+
+  const birthYear = birthYear.getFullYear();
+  const birthMonth = birthMonth.getMonth();
+  const birthDay = birthDay.getDate();
+};
