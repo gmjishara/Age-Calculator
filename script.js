465074e2a52fe696756cce727968cee7800ec8a9
gmjishara <janith971231@gmail.com>
2023-06-23 20:23:09 +0530

    set validate of birthday

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const ageCalculate = () => {
   const today = new Date();
-  const birthday = new Date(document.getElementById("birthday"));
+  const birthday = new Date(document.getElementById("birthday").value);
+ 
 
-  const birthYear = birthYear.getFullYear();
-  const birthMonth = birthMonth.getMonth();
-  const birthDay = birthDay.getDate();
+  const currentYear = today.getFullYear();
+  const currentMonth = today.getMonth() + 1;
+  const currentDay = today.getDate();
+
+  const birthYear = birthday.getFullYear();
+  const birthMonth = birthday.getMonth() + 1;
+  const birthDay = birthday.getDate();
+
+  if (
+    birthYear > currentYear ||
+    (birthYear === currentYear && birthMonth > currentMonth) ||
+    (birthMonth === currentMonth && birthDay > currentDay)
+  ) {
+    alert("not born yet");
+    return
+  }
+
+
 };
